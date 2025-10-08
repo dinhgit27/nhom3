49baf08face1dc3c48fadcd20838c0bf0717f1fb
Trương Tiến Anh <truongtienanhlhu@gmail.com>
2025-10-08 11:53:37 +0700

    classapi

--- Models/Class.cs
+++ Models/Class.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 
 namespace StudentManagementApi.Models
 {
-    public class Class
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Student> Students { get; set; } = new List<Student>();
-    }
+public class Class
+{
+    public int Id { get; set; }
+    public string ClassName { get; set; }
+
+    public List<Student> Students { get; set; }
+}
 }
